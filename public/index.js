1c794769425b5ae13a17b0921b5c82c1de022e6c
SHARP <ssharp9999@gmail.com>
2021-10-13 17:14:18 -0700

    service worker check

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+// service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+  })
+}
+
 let transactions = [];
 let myChart;
 
