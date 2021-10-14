e2bb414881bb1eea1b604fcec348b7d663517ee3
SHARP <ssharp9999@gmail.com>
2021-10-13 19:02:57 -0700

    server update for db folder

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const { join } = require('path')
 
+// local port 3000
 const PORT = 3000;
 
+// sync db
+const syncDB = require('./db')
+
+// create express app
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
+// middleware
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// sync the database and app to listen to heroku deoployed port or local port 3000
+syncDB()
+  .then(() => app.listen(process.env.PORT || PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  }))
+  .catch(err => console.log(err))
